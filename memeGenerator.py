e1903d643e7a2cc5f226c53e424e1e903e9529fe
Gary Chen <b07902063@ntu.edu.tw>
2022-06-14 21:40:06 +0800

    adapt memeGenerator to accept image id input

--- memeGenerator.py
+++ memeGenerator.py
@@ -8,7 +8,7 @@ def remove_file(name):
 	if os.path.exists(name):
 		os.remove(name)
 
-def memeGenerator(chinese_str:str, keyword:list or str=None,negative_keyword:list or str=None):
+def memeGenerator(chinese_str:str, keyword:list or str = None,negative_keyword:list or str = None, img_id:str = '0'):
 	generator = MemeTextGenerator('Phase1/vocabulary3000.txt')
 	result = generator.generate(chinese_str)
 	if result == None:
@@ -18,9 +18,9 @@ def memeGenerator(chinese_str:str, keyword:list or str=None,negative_keyword:lis
 		print("單字庫無匹配單字")
 		return
 	index = random.randint(0, len(result[0]) - 1)
-	img_arr = get_image_ndarray(result[1][index], keyword, negative_keyword)
-	putcaptions(result[0][index], img_arr)	
-	img_names = ["phase2.jpg", "my_image.png"]
+	img_arr = get_image_ndarray(result[1][index], keyword, negative_keyword, img_id)
+	putcaptions(result[0][index], img_arr, img_id)	
+	img_names = ['phase2_' + img_id + '.jpg', './phase3_tmp_' + img_id + '.png']
 	for name in img_names:
 		remove_file(name)
 
